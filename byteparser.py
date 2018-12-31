f8d1fcb3872b85e889ef532b18cd864d27a95432
Albert Borges <boalbe@microsoft.com>
2018-12-31 15:48:59 -0800

    Changing array bound characters from c++ chars to JavaScript chars.

--- byteparser.py
+++ byteparser.py
@@ -1,12 +1,12 @@
 file = open("gvmlhexdata.txt", "r") 
 str = file.read()
 bytearr = str.split(" ")
-code = "{"
+code = "["
 for byte in bytearr:
     code+="0x"
     code+=byte
     code+=","
 code = code[:len(code)-1]
-code+="}"
+code+="]"
 
 print code
